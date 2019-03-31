7c1fa4098a03b697536bcbd4500dbb3333039bd2
sachiket <sachiketbehera@gmail.com>
2019-04-01 02:28:27 +0530

    Adds Questionnaire Page

--- Vapors_app/models.py
+++ Vapors_app/models.py
@@ -1,9 +1,44 @@
 from django.db import models
 from django.contrib import auth
-# Create your models here.
+from django import forms
+from django.urls import reverse
+# Create your models here
+
 
 class User(auth.models.User,auth.models.PermissionsMixin):
 
-
     def __str__(self):
         return "@{}".format(self.username)
+
+
+# class Order(models.Model):
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=50)
+class BlogComments(models.Model):
+    f1 = models.CharField(max_length=100)
+    f2 = models.CharField(max_length=100)
+    f3 = models.CharField(max_length=100)
+    f4 = models.CharField(max_length=100)
+    f5 = models.CharField(max_length=100)
+    f6 = models.CharField(max_length=100)
+    f7 = models.CharField(max_length=100)
+    f8 = models.CharField(max_length=100)
+    f9 = models.CharField(max_length=100)
+    f10 = models.CharField(max_length=100)
+    f11 = models.CharField(max_length=100)
+    f12 = models.CharField(max_length=100)
+    f13 = models.CharField(max_length=100)
+    f14 = models.CharField(max_length=100)
+    f15 = models.CharField(max_length=100)
+    f16 = models.CharField(max_length=100)
+    f17 = models.CharField(max_length=100)
+    f18 = models.CharField(max_length=100)
+    f19 = models.CharField(max_length=100)
+    f20 = models.CharField(max_length=100)
+    f21 = models.CharField(max_length=100)
+    f22 = models.CharField(max_length=100)
+    f23 = models.CharField(max_length=100)
+    f24 = models.CharField(max_length=100)
+    f25 = models.CharField(max_length=100)
+    f26 = models.CharField(max_length=100)
+    f27 = models.CharField(max_length=100)
--- Vapors_app/views.py
+++ Vapors_app/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+import csv
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy #for url navigaton
 from django.views.generic import CreateView
+from django.views.generic import TemplateView
+from django.http import HttpResponse
 from . import forms
+from . import models
+from .models import BlogComments
+from .forms import BlogCommentsForm
 # Create your views here.
 
 class SignUp(CreateView):
@@ -9,3 +15,38 @@ class SignUp(CreateView):
     success_url = reverse_lazy('login')
     #redirect to login page
     template_name = 'accounts/signup.html'
+
+
+
+def showform(request):
+    form= BlogCommentsForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+
+    context= {'form': form }
+
+    return render(request, 'accounts/qform.html',context)
+
+
+
+
+def export_users_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="user.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['f1', 'f2', 'f3', 'f4','f5', 'f6', 'f7', 'f8','f9', 'f10', 'f11', 'f12','f13', 'f14', 'f15', 'f16','f17', 'f18', 'f19', 'f20','f21', 'f22', 'f23', 'f24','f25', 'f26', 'f27'])
+
+    users = BlogComments.objects.all()
+    for obj in users:
+        writer.writerow([obj.f1 ,obj.f2, obj.f3, obj.f4, obj.f5,obj.f6 ,obj.f7, obj.f8, obj.f9, obj.f10,obj.f11 ,obj.f12, obj.f13, obj.f14, obj.f15,obj.f16 ,obj.f17, obj.f18, obj.f19, obj.f20,obj.f21 ,obj.f22, obj.f23, obj.f24, obj.f25, obj.f26, obj.f27])
+
+    return response
+
+
+
+# class demo(CreateView):
+#     form_class = forms.UserForm
+#     success_url = reverse_lazy('login')
+#     template_name = 'accounts/qform.html'
